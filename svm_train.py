08ad66a6108210b5d8f1176215ecedace0c7542b
kingsman142 <kingsman142@yahoo.com>
2018-04-02 09:49:25 -0400

    added some mingda files and modified svm train file

--- svm_train.py
+++ svm_train.py
@@ -5,6 +5,8 @@ import scipy.stats
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 import math
+import collections
+from sklearn.preprocessing import normalize
 
 VIDEO_EFFECTIVE_RAW_FILE = "./annotations_videos/video/raw_result/video_Effective_raw.json"
 SENTIMENTS_CLEAN_FILE = "./annotations_videos/video/cleaned_result/video_Sentiments_clean.json"
@@ -53,25 +55,39 @@ train, test = train_test_split(video_ids, train_size = train_n, test_size = test
 
 train_topics = [topics_data_stats[x] for x in train if x in topics_data_stats]
 train_topics_out = [int(effective_data_stats[x]) for x in train if x in topics_data_stats]
+train_topics_out_counts = collections.Counter(train_topics_out)
 
 test_topics = [topics_data_stats[x] for x in test if x in topics_data_stats]
 test_topics_out = [int(effective_data_stats[x]) for x in test if x in topics_data_stats]
+test_topics_out_counts = collections.Counter(test_topics_out)
 
 train_sents = [sentiments_data_stats[x] for x in train if x in sentiments_data_stats]
 train_sents_out = [int(effective_data_stats[x]) for x in train if x in sentiments_data_stats]
+train_sents_out_counts = collections.Counter(train_sents_out)
 
 test_sents = [sentiments_data_stats[x] for x in test if x in sentiments_data_stats]
 test_sents_out = [int(effective_data_stats[x]) for x in test if x in sentiments_data_stats]
+test_sents_out_counts = collections.Counter(test_sents_out)
 
-topics_SVM = SVC(kernel='rbf', degree=3)
-print(train_topics[0].shape)
-print(len(train_topics_out))
+topics_SVM = SVC(kernel='linear', degree=3)
+#print(train_topics[0].shape)
+#print(len(train_topics_out))
 topics_SVM.fit(train_topics, train_topics_out)
 topics_score = topics_SVM.score(test_topics, test_topics_out)
+print(topics_SVM.n_support_[2])
+print(train_topics_out_counts[2])
+print(topics_SVM.n_support_)
+print(train_topics_out_counts)
+class_support_vectors_topics = normalize([[topics_SVM.n_support_[i] / train_topics_out_counts[i+1] for i in range(0, len(topics_SVM.n_support_))]], norm='l1')
+print("Topics SVM Support Vectors: %s" % (class_support_vectors_topics))
+print("Weights: %s" % (topics_SVM.coef_))
 
-sents_SVM = SVC(kernel='rbf')
+sents_SVM = SVC(kernel='linear')
 sents_SVM.fit(train_sents, train_sents_out)
 sents_score = sents_SVM.score(test_sents, test_sents_out)
+class_support_vectors_sentiments = normalize([[sents_SVM.n_support_[i] / train_sents_out_counts[i+1] for i in range(0, len(sents_SVM.n_support_))]], norm='l1')
+print("Sentiments SVM Support Vectors: %s" % (class_support_vectors_sentiments))
+print("Weights: %s" % (sents_SVM.coef_))
 
 print("Number of video ids: %d" % (len(video_ids)))
 print("Topics score: %.4f" % (topics_score))
